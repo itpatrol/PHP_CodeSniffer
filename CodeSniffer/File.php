88c9b934e2afd67a9ca85b1f8a789054f2719d1d
Gor Martsen <Gor.martsen@gmail.com>
2015-08-24 16:32:00 -0400

    Add KEY for DEEP

--- CodeSniffer/File.php
+++ CodeSniffer/File.php
@@ -1422,16 +1422,17 @@ class PHP_CodeSniffer_File
     }//end getErrors()    
 
     public function saveStackChanges($old_content, $new_content){
-      if(PHP_CODESNIFFER_CBF){
+      if($deep = getenv('DEEP')){
         // We do not compare here. Skip.
         return;
       }
+      echo "We are here\n";
       file_put_contents($this->_file.'.before', $old_content);
       file_put_contents($this->_file, $new_content);
       // Call phpcbf with the same standard and changed file. SHA=0000000 to get changes for changed lines
       $values = $this->phpcs->cli->getCommandLineValues();
       $standards = implode(",",$values['standard']);
-      exec('SHA=0000000000000000000000000000000000000000 phpcbf --standard=' . $standards . ' ' . $this->_file);
+      exec('DEEP=TRUE SHA=0000000000000000000000000000000000000000 phpcbf --standard=' . $standards . ' ' . $this->_file);
       exec('diff -u ' . $this->_file.'.before' . ' ' . $this->_file, $result);
       $this->_stack[count($this->_stack) - 1]['changes'][] = $result;
       
